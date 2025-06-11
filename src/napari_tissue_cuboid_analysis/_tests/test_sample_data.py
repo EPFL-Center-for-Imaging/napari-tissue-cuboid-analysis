134f31cbd3e520d3552cae046baf9e673d7e1724
Bastien Chesaux Dapia <bastien.chesauxdapia@epfl.ch>
2025-06-11 17:08:37 +0200

    remove unimplemented tests

--- src/napari_tissue_cuboid_analysis/_tests/test_sample_data.py
+++
@@ -1,7 +0,0 @@
-# from napari_tissue_cuboid_analysis import make_sample_data
-
-# add your tests here...
-
-
-def test_something():
-    pass
--- src/napari_tissue_cuboid_analysis/_tests/test_writer.py
+++
@@ -1,7 +0,0 @@
-# from napari_tissue_cuboid_analysis import write_single_image, write_multiple
-
-# add your tests here...
-
-
-def test_something():
-    pass
